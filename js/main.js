6f3c4cefb4e2ec457a288c3addfaab8c3cd91cc5
AbdulrhimElsayed <abdulrhimelsayed@gmail.com>
2026-05-01 22:27:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
-let statue=1;
-let arrowLeft=document.getElementById("arrow-left"); 
-let arrowRight=document.getElementById("arrow-right");
-arrowLeft.addEventListener("click",()=>{
-    if(statue==1){
-        statue=3;
-    }else{
-        statue--;
-    }
-})
-arrowRight.addEventListener("click",()=>{
-    if(statue==3){
-        statue=1;
-    }else{
-        statue++;
-    }
-})
-function sliderChange(){
+let statue = 2;
+let arrowLeft = document.getElementById("arrow-left");
+let arrowRight = document.getElementById("arrow-right");
+let controlShow = document.querySelector(".control-show");
+arrowLeft.addEventListener("click", () => {
+  if (statue == 1) {
+    statue = 3;
+  } else {
+    statue--;
+  }
+console.log(statue);
+sliderChange();
+  
+});
+arrowRight.addEventListener("click", () => {
+  if (statue == 3) {
+    statue = 1;
+  } else {
+    statue++;
+  }
+console.log(statue);
+sliderChange();
 
-} 
+});
+function sliderChange() {
+  controlShow.classList.remove("case-1", "case-2");
+
+  if (statue == 1) {
+    controlShow.classList.add("case-1");
+  } else if (statue == 3) {
+    controlShow.classList.add("case-2");
+  }
+}
+sliderChange();
+console.log(statue);

b171de805a1d32eb5c71bb293a6f09e17faf578e
AbdulrhimElsayed <abdulrhimelsayed@gmail.com>
2026-05-04 20:21:09 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let statue = 2;
 let arrowLeft = document.getElementById("arrow-left");
 let arrowRight = document.getElementById("arrow-right");
 let controlShow = document.querySelector(".control-show");
+let sliderContent = document.getElementById("slider");
 arrowLeft.addEventListener("click", () => {
   if (statue == 1) {
     statue = 3;
@@ -10,6 +11,7 @@ arrowLeft.addEventListener("click", () => {
   }
 console.log(statue);
 sliderChange();
+sliderContentChange();
   
 });
 arrowRight.addEventListener("click", () => {
@@ -25,11 +27,13 @@ sliderContentChange();
 });
 function sliderChange() {
   controlShow.classList.remove("case-1", "case-2");
-
+  sliderContent.classList.remove("case-1", "case-2", );
   if (statue == 1) {
     controlShow.classList.add("case-1");
+    sliderContent.classList.add("case-1");
   } else if (statue == 3) {
     controlShow.classList.add("case-2");
+    sliderContent.classList.add("case-2");
   }
 }
 sliderChange();
@@ -42,13 +46,22 @@ menuIcon.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 let contentImage = document.getElementById("content-image");
+let slider = document.getElementById("slider");
 function sliderContentChange() {
+  // امسح أي كلاس قديم
+  // slider.classList.remove("slider1", "slider2", "slider3");
+
   if (statue == 1) {
-    contentImage.innerHTML = "Discover innovative <br /> Ways to decorate";
+    contentImage.innerHTML = "Discover innovative<br /> Ways to decorate";
+    // slider.classList.add("slider1");
+
   } else if (statue == 2) {
-    contentImage.innerHTML = "We are available all <br /> across the globe";
+    contentImage.innerHTML = "We are available all<br /> across the globe";
+    // slider.classList.add("slider2");
+
   } else if (statue == 3) {
-    contentImage.innerHTML = "Discover innovative <br /> Ways to decorate";
+    contentImage.innerHTML =  "Discover innovative<br /> Ways to decorate";
+    // slider.classList.add("slider2");
   }
 }
 sliderContentChange();
